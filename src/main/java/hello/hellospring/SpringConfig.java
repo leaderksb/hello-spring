9f73a7585c00747bf04383581b1ebd26033e3675
김수빈 <leaderksb@naver,com>
2022-11-24 00:03:00 +0900

    스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -2,18 +2,21 @@ package hello.hellospring;
 
 import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.sql.DataSource;
 
 @Configuration  // 스프링이 뜰 때 컨피그레이션을 읽어오고
 public class SpringConfig {
 
+    private final MemberRepository memberRepository;
+
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
 //    @PersistenceContext
+
+    /*
     private EntityManager em;
 
     @Autowired
@@ -21,7 +24,7 @@ public class SpringConfig {
         this.em = em;
     }
 
-    /*
+
     private DataSource dataSource;
 
     @Autowired
@@ -32,14 +35,19 @@ public class SpringConfig {
 
     @Bean  // 스프링 빈에 등록하란 뜻이네 하고 인식
     public MemberService memberService() {  // MemberService 로직을 호출해서 등록해줌
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
+//    @Bean  // 스프링 빈에 등록하란 뜻이네 하고 인식
+//    public MemberService memberService() {  // MemberService 로직을 호출해서 등록해줌
+//        return new MemberService(memberRepository());
+//    }
+
+//    @Bean
+//    public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
 //        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+//        return new JpaMemberRepository(em);
+//    }
 }
